c7533d292c9eeed30b156e4eccfdf64401f666ad
Shahin1723 <79781718+Shahin1723@users.noreply.github.com>
2022-01-29 12:01:05 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,12 @@
+xyz = ['Name','Place','Year','CEO']
+xyz.sort()
+print(xyz)
+input = input("Enter any data to check from the above list :")
+if (input == 'Name'):
+    print("Name is : XYZ")
+elif (input == 'Place'):
+    print("Place is : Kerala")
+elif (input == 'Year'):
+    print("Year is : 2022")
+elif (input == "CEO"):
+    print("CEO is : Shahin")
